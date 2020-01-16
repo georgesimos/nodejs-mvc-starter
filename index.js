c728ee4880879121fc60edbd8a511ca88afb51d6
George <admin@georgesimos.com>
2020-01-16 20:48:52 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+ *
+ * Author:  George Simos - georgesimos.com
+ *
+ * License: Copyright (c) 2019 George Simos
+ * @link https://github.com/georgesimos/nodejs-starter
+ *
+ */
+
+import express from 'express';
+import logger from 'morgan';
+import dotenv from 'dotenv';
+import expressStatusMonitor from 'express-status-monitor';
+import connectDB from './src/config/mongoose';
+import routes from './src/routes';
+
+// Make all variables from our .env file available in our process
+dotenv.config({ path: '.env.example' });
+
+// Init express server
+const app = express();
+
+// Connect to MongoDB.
+connectDB();
+
+// Middlewares & configs setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.disable('x-powered-by');
+app.use(expressStatusMonitor());
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
+// Here we define the api routes
+app.use(routes);
+
+const port = process.env.PORT || 8080;
+const address = process.env.SERVER_ADDRESS || 'localhost';
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Server running on http://${address}:${port}`));
