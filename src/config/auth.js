3600ed13a19fcb94017abffe926e85e8ead33d5b
George <admin@georgesimos.com>
2020-01-16 20:49:05 +0200

    auth middleware

---
+++ src/config/auth.js
@@ -0,0 +1,24 @@
+/**
+ * Auth middleware
+ */
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token,
+    });
+    if (!user) throw new Error();
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send({ error: 'Please authenticate.' });
+  }
+};
+
+export default auth;
